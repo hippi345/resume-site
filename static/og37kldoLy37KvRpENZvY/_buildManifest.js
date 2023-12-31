988a8195a5c7fab7b539ae0935d8f11fd8b579f0
hippi345 <vision2020joel@gmail.com>
2023-12-31 01:36:22 +0000

    Deploying to deploy from @ hippi345/resume-site@5f23e2ab73127204e80421346a17f9f46e4d4f83 🚀

--- static/og37kldoLy37KvRpENZvY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/953-d68f8b8dbd48b9bc.js","static/chunks/pages/index-3000af1a21f7dc50.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
